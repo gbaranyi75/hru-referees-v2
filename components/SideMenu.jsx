2c52153d268e87480f1376b8238740635bd18558
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-06-18 16:18:04 +0200

    fixed buttons10

---
+++ components/SideMenu.jsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaTable, FaFootballBall, FaVideo, FaUsers } from "react-icons/fa";
+
+const SideMenu = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="mb-1 h-full px-3 overflow-y-auto border-r-2">
+      <ul className="space-y-2 font-medium">
+        <li>
+          <div className="w-full mb-1 px-4 md:px-0 text-center">
+            <Link
+              className={`${
+                pathname === "/dashboard/calendar" ? "bg-gray-300" : ""
+              } w-full items-center inline-flex py-3 text-gray-600 hover:text-gray-700 hover:bg-gray-300 text-sm font-medium rounded-md`}
+              href="/dashboard/calendar"
+            >
+              <FaTable color="gray" size={20} className="mx-3" />
+              <span>Táblázatok</span>
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div className="w-full mb-1 px-4 md:px-0 text-center">
+            <Link
+              className={`${
+                pathname === "/merkozesek" ? "bg-gray-300" : ""
+              } w-full items-center inline-flex py-3 text-gray-600 hover:text-gray-700 hover:bg-gray-300 text-sm font-medium rounded-md`}
+              href="/merkozesek"
+            >
+              <FaFootballBall color="gray" size={20} className="mx-3" />
+              <span>Mérkőzések</span>
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div className="w-full mb-1 px-4 md:px-0 text-center">
+            <Link
+              className={`${
+                pathname === "/" ? "bg-gray-300" : ""
+              } w-full items-center inline-flex py-3 text-gray-600 hover:text-gray-700 hover:bg-gray-300 text-sm font-medium rounded-md`}
+              href="/merkozesek"
+            >
+              <FaUsers color="gray" size={20} className="mx-3" />
+              <span>Játékvezetők</span>
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div className="w-full mb-1 px-4 md:px-0 text-center">
+            <Link
+              className={`${
+                pathname === "/merkozesek" ? "bg-gray-300" : ""
+              } w-full items-center inline-flex py-3 text-gray-600 hover:text-gray-700 hover:bg-gray-300 text-sm font-medium rounded-md`}
+              href="/merkozesek"
+            >
+              <FaVideo color="gray" size={20} className="mx-3" />
+              <span>Videók</span>
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideMenu;
