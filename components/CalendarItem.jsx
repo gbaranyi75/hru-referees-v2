475e7a4bc0d1aa5c417a6a0ee4ec86897bf6325b
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-06-19 13:30:11 +0200

    deleted React imports

--- components/CalendarItem.jsx
+++ components/CalendarItem.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import { MdOutlineExpandMore, MdOutlineExpandLess } from "react-icons/md";
 import DisabledButton from "@/components/common/DisabledButton";
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,3 @@
-'use client';
-
-import React from 'react'
-
 const Footer = () => {
   const year = new Date().getFullYear();
 
