aa0ed5ec721e733cd70e8bfefefc1dc44081c087
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-10-02 16:45:12 +0200

    added password-reset flow finished2

--- actions/updatePassword.js
+++ actions/updatePassword.js
@@ -5,13 +5,15 @@ import User from "@/models/User";
 import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
 
-export async function updatePassword({ password, token }) {
+export async function updatePassword({ newPassword, token }) {
   await connectDB();
   const salt = await bcrypt.genSalt(8);
-  const passwordHashed = await bcrypt.hash(password, salt);
-  await User.findOneAndUpdate(
+  const passwordHashed = await bcrypt.hash(newPassword, salt);
+
+  const res = await User.findOneAndUpdate(
     { verifyToken: token },
     { password: passwordHashed }
   );
-  redirect("/auth/belepes");
+  console.log(res)
+  if (res) redirect("/auth/belepes");
 }
