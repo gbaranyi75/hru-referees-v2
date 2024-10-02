b73c551e510bea97615e18181060e7dc62bdfd74
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-10-02 11:54:01 +0200

    added password-reset flow init3

--- models/User.js
+++ models/User.js
@@ -25,6 +25,10 @@ const UserSchema = new Schema(
       type: String,
       required: [true, "Username is required"],
     },
+    verifyToken: {
+      type: String,
+      required: false,
+    }
   },
   {
     timestamps: true,
