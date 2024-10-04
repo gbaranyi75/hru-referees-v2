4da3efcd3aa7a5523ed3e96c51dffd25f7dd06dd
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-10-04 09:33:51 +0200

    fixed respone

--- actions/mailAction.js
+++ actions/mailAction.js
@@ -5,7 +5,6 @@ import User from "@/models/User";
 import { nanoid } from "nanoid";
 import nodemailer from "nodemailer";
 import { Resend } from "resend";
-//import ResetPasswordEmailMsg from "@/components/ResetPasswordEmailMsg";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -19,28 +18,32 @@ export async function mailAction({ email, domain }) {
       port: 587,
       secure: false,
       auth: {
-        user: process.env.MAILER_USER, //"hrureferees.hu@gmail.com",
-        pass: process.env.MAILER_PASS, //"fmlutfdtwaclqcly",
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS,
       },
     });
     console.log("Token ", token);
     const htmlBody = `Kattints ide a jelszó  helyreállításához! <a href="${domain}/jelszo-helyreallitas/${token}"> Jelszó helyreállítása</a>`;
-    //const htmlBody = <ResetPasswordEmailMsg domain={domain} token={token} />;
     const message = {
-      from: '"HRU Referees" <bgabor91@gmail.com>', // sender address
-      to: email, // list of receivers
-      subject: "Jelszó helyreállítás", // Subject line
-      text: "Új jelszó", // plain text body
-      html: htmlBody, // html body
+      from: '"HRU Referees" <bgabor91@gmail.com>',
+      to: email,
+      subject: "Jelszó helyreállítás",
+      text: "Új jelszó",
+      html: htmlBody,
     };
 
     const info = await transporter.sendMail(message);
-    const res = await User.findOneAndUpdate(
-      { email: email },
-      { verifyToken: token }
-    );
-    console.log(res.verifyToken);
-    return res.verifyToken;
+    console.log(info.accepted, info.response);
+    console.log(info.response);
+
+    if (info.accepted) {
+      await User.findOneAndUpdate(
+        { email: email },
+        { verifyToken: token }
+      );
+    }
+
+    return info.accepted;
   } else {
     console.log("user does not exist", result);
     return result;
