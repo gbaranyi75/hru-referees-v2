287553d03044a5519d132f3f70a26865e62ff48a
Gabor Baranyi <v-gbaranyi@expediagroup.com>
2024-10-04 08:55:01 +0200

    setup brevo

--- actions/mailAction.js
+++ actions/mailAction.js
@@ -4,6 +4,10 @@ import connectDB from "@/config/database";
 import User from "@/models/User";
 import { nanoid } from "nanoid";
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
+import ResetPasswordEmailMsg from "@/components/ResetPasswordEmailMsg";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function mailAction({ email, domain }) {
   await connectDB();
@@ -11,9 +15,9 @@ export async function mailAction({ email, domain }) {
   if (result) {
     const token = nanoid(32);
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAILER_USER, //"hrureferees.hu@gmail.com",
         pass: process.env.MAILER_PASS, //"fmlutfdtwaclqcly",
@@ -21,6 +25,7 @@ export async function mailAction({ email, domain }) {
     });
     console.log("Token ", token);
     const htmlBody = `Kattints ide a jelszó  helyreállításához! <a href="${domain}/jelszo-helyreallitas/${token}"> Jelszó helyreállítása</a>`;
+    //const htmlBody = <ResetPasswordEmailMsg domain={domain} token={token} />;
     const message = {
       from: '"HRU Referees" <bgabor91@gmail.com>', // sender address
       to: email, // list of receivers
@@ -29,18 +34,12 @@ export async function mailAction({ email, domain }) {
       html: htmlBody, // html body
     };
 
-    transporter.sendMail(message, (err, info) => {
-      if (err) {
-        console.log("Error occurred. " + err.message);
-        return process.exit(1);
-      }
-      console.log("Message sent: %s", info.messageId);
-    });
-
+    const info = await transporter.sendMail(message);
     const res = await User.findOneAndUpdate(
       { email: email },
       { verifyToken: token }
     );
+    console.log(res.verifyToken);
     return res.verifyToken;
   } else {
     console.log("user does not exist", result);
